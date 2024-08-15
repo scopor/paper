eb53c985a3266c28c593e199886e9b6146acb4b6
scopor <39816569+scopor@users.noreply.github.com>
2024-08-15 20:55:15 +0800

    Create Footer.vue

---
+++ components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="pt-10 bg-gray-100 text-black text-center">
+    <p class="text-sm text-gray-600">Designed by <a class="text-pink-400" href="https://sooloe.com/ ">Scopor</a>, Proudly published with Vercel.</p>
+    <p class="text-zinc-700 font-sans mt-8">虚无之地</p>
+  </footer>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Footer',
+}
+</script>
+
+<style scoped>
+/* 可以在这里添加更多样式 */
+</style>
