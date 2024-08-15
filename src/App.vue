41d7364f59a0f5b8c6e1ab31395aa2bfe4c85be4
scopor <39816569+scopor@users.noreply.github.com>
2024-08-15 20:58:55 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex flex-col justify-items-center items-center">
+    <div class="flex flex-col sm:w-auto md:w-2/5 bg-white my-16">
+      <div class="shadow-md mb-1">
+        <NavBar></NavBar>
+        <router-view/>
+      </div>
+      <Footer></Footer>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import NavBar from './components/NavBar.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+    Footer,
+  },
+}
+</script>
+
+<style>
+/* 可以在这里添加全局样式 */
+</style>
