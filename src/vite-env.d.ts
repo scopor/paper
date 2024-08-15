11b9149f7aa03f556353c1f5bfe325ef5cbbe73e
scopor <39816569+scopor@users.noreply.github.com>
2024-08-15 21:03:39 +0800

    Create vite-env.d.ts

---
+++ src/vite-env.d.ts
@@ -0,0 +1,11 @@
+import Vue from '@vitejs/plugin-vue'
+import Markdown from 'vite-plugin-md'
+
+export default {
+    plugins: [
+        Vue({
+            include: [/\.vue$/, /\.md$/], // <--
+        }),
+        Markdown(),
+    ],
+}
