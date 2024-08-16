a7250213ab68ed3b8f0d5a8be3ec3a9975c3cea5
scopor <39816569+scopor@users.noreply.github.com>
2024-08-16 09:45:01 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import router from './router'
 import './style.css'
 import './dracula.css'
 import 'github-markdown-css/github-markdown-light.css'
-import 'highlight.js/styles/xcode.min.css'
-// import 'highlight.js/styles/rainbow.css'
+import 'highlight.js/styles/github.css'
 import 'css.gg/icons/icons.css'
+
 import App from './App.vue'
 
 createApp(App).use(router).mount('#app')
