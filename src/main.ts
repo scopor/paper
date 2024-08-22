a2b933dde199231b9ff380243d2a8b78e9ecd7a0
scopor <39816569+scopor@users.noreply.github.com>
2024-08-22 09:13:17 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import router from './router'
+import {createPinia} from "pinia";
 import './style.css'
 import './dracula.css'
-import 'github-markdown-css/github-markdown-light.css'
+import 'github-markdown-css/github-markdown.css'
 import 'highlight.js/styles/xcode.min.css'
 import 'css.gg/icons/icons.css'
+import 'markdown-it-copy-code/styles/base.css'
+import 'markdown-it-copy-code/styles/medium.css'
+import 'prismjs/themes/prism-coy.css'
 import App from './App.vue'
 
-createApp(App).use(router).mount('#app')
+export const pinia = createPinia()
+
+createApp(App).use(router).use(pinia).mount('#app')
