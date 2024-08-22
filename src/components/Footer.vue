d220b7c4e42626db37d3ce128ba59ff7cc782ae8
scopor <39816569+scopor@users.noreply.github.com>
2024-08-22 10:30:04 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="pt-10 bg-gray-100 text-black text-center">
     <p class="text-sm text-gray-600">Designed by <a class="text-pink-400" href="https://sooloe.com/ ">Scopor</a>, Proudly published with Vercel.</p>
-    <p class="text-zinc-700 font-sans mt-8">虚无之地</p>
+    <p class="text-zinc-700 font-sans my-8">虚无之地</p>
   </footer>
 </template>
 
