f877f314782258186a292ac1ec7fba3dcd8e78f6
scopor <39816569+scopor@users.noreply.github.com>
2024-08-26 10:51:33 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,14 +18,7 @@ export default defineConfig({
                 }
             },
             plugins: [
-                viteCompression({
-                    verbose: true, // 是否在控制台中输出压缩结果
-                    disable: false,
-                    threshold: 1024000, // 如果体积大于阈值，将被压缩，单位为b，体积过小时请不要压缩，以免适得其反
-                    algorithm: 'gzip', // 压缩算法，可选['gzip'，' brotliccompress '，'deflate '，'deflateRaw']
-                    ext: '.gz',
-                    deleteOriginFile: true
-                })
+                
             ]
         },
         terserOptions: {
