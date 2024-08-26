b8bbfd19bff37199fb99d3e70ad5b3840afbf298
scopor <39816569+scopor@users.noreply.github.com>
2024-08-26 10:04:52 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,48 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {nodePolyfills} from "vite-plugin-node-polyfills";
+import viteCompression from "vite-plugin-compression"
 
 export default defineConfig({
     build: {
-        chunkSizeWarningLimit: 1500,
+        sourcemap: false,
+        minify: "terser",
+        chunkSizeWarningLimit: 10240,
         rollupOptions: {
             output: {
+                chunkFileNames: "static/js/[name]-[hash].js",
+                entryFileNames: "static/js/[name]-[hash].js",
+                assetFileNames: "static/[ext]/[name]-[hash].[ext]",
                 manualChunks(id) {
                     if (id.includes('node_modules')) {
-                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                        return "vendor";
                     }
                 }
             },
+            plugins: [
+                viteCompression({
+                    verbose: true, // 是否在控制台中输出压缩结果
+                    disable: false,
+                    threshold: 1024000, // 如果体积大于阈值，将被压缩，单位为b，体积过小时请不要压缩，以免适得其反
+                    algorithm: 'gzip', // 压缩算法，可选['gzip'，' brotliccompress '，'deflate '，'deflateRaw']
+                    ext: '.gz',
+                    deleteOriginFile: true
+                })
+            ]
         },
+        terserOptions: {
+            compress: {
+                drop_console: true,
+                drop_debugger: true
+            }
+        }
     },
     optimizeDeps: {
         esbuildOptions: {
             define: {
                 global: 'globalThis', // 将 global 定义为 globalThis
             },
-            plugins: [
-
-            ],
+            plugins: [],
         },
     },
     plugins: [
