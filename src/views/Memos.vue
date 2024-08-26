1b84dbcbc4d39ea835624d170bd58069ebd4b2bc
scopor <39816569+scopor@users.noreply.github.com>
2024-08-26 17:38:28 +0800

    Update Memos.vue

--- src/views/Memos.vue
+++ src/views/Memos.vue
@@ -15,7 +15,7 @@
             </div>
             <div class="flex justify-end">
               <div class="text-sm flex items-center">
-                <span class="mb-0.5 pr-1">❤️</span>
+                <span class="lg:mb-0.5 pr-1">❤️</span>
                 <span class="hover:text-pink-400">{{ formatGistDate(gist.created_at) }}</span></div>
             </div>
           </div>
