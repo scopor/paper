f4ce334375c9cd84ff7b52a44bb51acacf44bf6e
scopor <39816569+scopor@users.noreply.github.com>
2024-09-19 17:21:09 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <template>
-  <div class="flex flex-col justify-items-center items-center">
-    <div class="flex flex-col lg:w-2/5 w-full bg-white shadow-md">
-      <div class="sticky top-0 z-50 bg-white">
-        <NavBar></NavBar>
-      </div>
-      <router-view id="Page"/>
-      <Footer></Footer>
+  <div class="container max-w-5xl bg-white min-h-screen shadow-md mx-auto" id="shadowApp">
+    <div class="sticky top-0 z-50 bg-white">
+      <NavBar></NavBar>
     </div>
+    <router-view/>
+    <Footer></Footer>
   </div>
 </template>
 
@@ -16,5 +14,4 @@ import Footer from './components/Footer.vue';
 </script>
 
 <style>
-/* 可以在这里添加全局样式 */
 </style>
